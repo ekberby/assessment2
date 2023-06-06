db4a4068ae194c3d4a473de6d284f8508815d69d
ekberby <ekber.ebilov.2001@mail.ru>
2023-06-06 04:39:03 +0100

    Designed main page

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import tkinter
 
 window = tkinter.Tk()
 
-window.geometry("500x300")
+window.geometry("300x200")
 
-label = tkinter.Label(window, text = "Please enter your username:", font="Arial, 20")
+frame1 = tkinter.Frame(window)
 
-input = tkinter.Entry(window)
+label = tkinter.Label(window, text = "Please enter your username:", font="Arial, 12")
 
-button = tkinter.Button(text="Log in", bg="green", fg="white")
+input = tkinter.Entry(frame1)
 
-label.pack()
+button = tkinter.Button(frame1, text="Log in", bg="green", fg="white")
 
-input.pack()
+label.pack(pady=20)
 
-button.pack()
+frame1.pack()
+
+input.grid(row = 1, column=0, padx=5, pady=5)
+
+button.grid(row=1, column=1, padx=5, pady=5)
 
 window.title("Best Game Ever")
 
